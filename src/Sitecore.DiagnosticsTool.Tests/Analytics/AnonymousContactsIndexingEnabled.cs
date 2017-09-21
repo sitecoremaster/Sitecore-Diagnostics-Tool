393424c03531514ea96a0422d924895933d16df7
DK\ach <ach@sitecore.net>
2017-09-21 16:49:44 +0300

    Test KB 171238

---
+++ src/Sitecore.DiagnosticsTool.Tests/Analytics/AnonymousContactsIndexingEnabled.cs
@@ -0,0 +1,40 @@
+﻿using Sitecore.Diagnostics.Base;
+using Sitecore.Diagnostics.Objects;
+using Sitecore.DiagnosticsTool.Core.Categories;
+using Sitecore.DiagnosticsTool.Core.Resources.Logging;
+using Sitecore.DiagnosticsTool.Core.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.DiagnosticsTool.Tests.Analytics
+{
+    class AnonymousContactsIndexingEnabled : KbTest
+    {
+
+        protected string SettingName = "ContentSearch.Analytics.IndexAnonymousContacts";
+        public override string KbNumber => "171238";
+
+        public override string KbName { get; } = "Issues caused by indexing anonymous contacts";
+
+        public override IEnumerable<Category> Categories { get; } = new[] { Category.Analytics };
+
+        protected override bool IsActual(ISitecoreVersion sitecoreVersion)
+        {
+            return sitecoreVersion.MajorMinorUpdateInt >= 812;
+        }
+
+        public override void Process(ITestResourceContext data, ITestOutputContext output)
+        {
+            Assert.ArgumentNotNull(data, nameof(data));
+
+            if (data.SitecoreInfo.GetBoolSetting(SettingName))
+            {
+                 Report(output);
+            }
+        }
+    }
+}
+
