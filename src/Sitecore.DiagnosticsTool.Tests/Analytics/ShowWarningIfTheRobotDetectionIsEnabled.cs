e70b5f9e711f927c69930e45f7602451e5b8ec7a
DK\VIAF <viaf@sitecore.net>
2017-09-28 12:08:02 +0300

    ShowWarningIfTheRobotDetectionIsEnabled Test

---
+++ src/Sitecore.DiagnosticsTool.Tests/Analytics/ShowWarningIfTheRobotDetectionIsEnabled.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.DiagnosticsTool.Core.Categories;
+using Sitecore.DiagnosticsTool.Core.Tests;
+
+namespace Sitecore.DiagnosticsTool.Tests.Analytics
+{
+    public class ShowWarningIfTheRobotDetectionIsEnabled : Test
+    {
+        protected const string ErrorMessage = "Sitecore automatic robot detection functionality is disabled. This functionality filters out unwanted interactions from automated browsers and robots. Sitecore recommends enabling 'Analytics.AutoDetectBots' setting to reduce the growth of the xDB";
+        
+        public override string Name => "Sitecore robot detection functionality is disabled.";
+
+        public override IEnumerable<Category> Categories => new[] { Category.Analytics };
+
+        protected override bool IsActual(ITestResourceContext data)
+        {
+            return !data.SitecoreInfo.GetBoolSetting("Analytics.AutoDetectBots");
+        }
+
+        public override void Process(ITestResourceContext data, ITestOutputContext output)
+        {
+            output.Warning(ErrorMessage);
+        }
+    }
+}
