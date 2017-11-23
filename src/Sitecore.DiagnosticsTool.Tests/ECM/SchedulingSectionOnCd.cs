b318a5149ef06a4ce689b08e0926bdd7d2853a5f
Alen Pelin <alenpelin@gmail.com>
2017-11-23 15:34:04 +1100

    Fix SchedulingSectionOnCd test on single instance (closes #39)

--- src/Sitecore.DiagnosticsTool.Tests/ECM/SchedulingSectionOnCd.cs
+++ src/Sitecore.DiagnosticsTool.Tests/ECM/SchedulingSectionOnCd.cs
@@ -1,6 +1,7 @@
 ﻿namespace Sitecore.DiagnosticsTool.Tests.ECM
 {
   using System.Collections.Generic;
+  using System.Linq;
 
   using JetBrains.Annotations;
 
@@ -11,13 +12,16 @@
   public class SchedulingSectionOnCd : Test
   {
     public override IEnumerable<Category> Categories { get; } = new[] { Category.Ecm };
-
-    public override IEnumerable<ServerRole> ServerRoles => new[] { ServerRole.ContentDelivery };
-
-    public override string Name { get; } = "ECM scheduling section must be disabled on CD";
+    
+    public override string Name { get; } = "EXM scheduling section must be disabled on ContentDelivery instance";
 
     [NotNull]
-    protected string ErrorMessage => "ECM agents found in scheduling section. These agents should be disabled on CD.";
+    protected string ErrorMessage => "EXM agents found in scheduling section. These agents should be disabled on ContentDelivery instance.";
+
+    protected override bool IsActual(IReadOnlyCollection<ServerRole> roles)
+    {
+      return roles.Contains(ServerRole.ContentDelivery) && !roles.Contains(ServerRole.ContentManagement);
+    }
 
     public override void Process(ITestResourceContext data, ITestOutputContext output)
     {
