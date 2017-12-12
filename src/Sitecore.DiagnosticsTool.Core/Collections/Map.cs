f8be943e496f6ee78b57f2cb4da6e2280d0988c1
Alen Pelin <alenpelin@gmail.com>
2017-12-12 14:35:27 +1100

    Improve map

--- src/Sitecore.DiagnosticsTool.Core/Collections/Map.cs
+++ src/Sitecore.DiagnosticsTool.Core/Collections/Map.cs
@@ -1,14 +1,25 @@
 ﻿namespace Sitecore.DiagnosticsTool.Core.Collections
 {
+  using System;
   using System.Collections.Generic;
   using System.Runtime.Serialization;
 
   using JetBrains.Annotations;
 
+  using Sitecore.Diagnostics.Base;
+
   public class Map : Dictionary<string, string>
   {
+    [CanBeNull]
+    private Func<string, string> KeyInitializer { get; }
+
     public Map()
     {
+    }
+
+    public Map(Func<string, string> keyInitializer)
+    {
+      KeyInitializer = keyInitializer;
     }
 
     public Map(int capacity)
@@ -40,14 +51,29 @@
       : base(info, context)
     {
     }
+
+    public void Add(string value)
+    {
+      Assert.IsNotNull(KeyInitializer);
+
+      Add(KeyInitializer(value), value);
+    }
   }
 
   public class Map<T> : Dictionary<string, T>
   {
+    [CanBeNull]
+    private Func<T, string> KeyInitializer { get; }
+
     public Map()
     {
     }
 
+    public Map(Func<T, string> keyInitializer)
+    {
+      KeyInitializer = keyInitializer;
+    }
+    
     public Map(int capacity)
       : base(capacity)
     {
@@ -76,6 +102,13 @@
     protected Map(SerializationInfo info, StreamingContext context)
       : base(info, context)
     {
+    }
+
+    public void Add(T value)
+    {
+      Assert.IsNotNull(KeyInitializer);
+
+      Add(KeyInitializer(value), value);
     }
   }
 }
